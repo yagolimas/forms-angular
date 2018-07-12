11fd8f0c288252f761468b51a927cc2b8437431e
yschuenck <yago.schuenck@gmail.com>
2018-07-12 00:22:10 -0300

    Create Component DataForm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DataFormComponent } from './data-form/data-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'data-form', component: DataFormComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataFormModule } from './data-form/data-form.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    DataFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
