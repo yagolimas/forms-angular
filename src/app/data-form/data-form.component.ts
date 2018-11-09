f9426b352e38db435839f782d158be183bb31eed
yschuenck <yago.schuenck@gmail.com>
2018-11-09 01:16:02 -0200

    Create Form Validations

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+//import { HttpClient } from '@angular/common/http';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-data-form',
@@ -10,9 +13,140 @@ export class DataFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: Http
+  ) { }
 
   ngOnInit() {
+
+    this.form = this.formBuilder.group({
+      
+      nome:  [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+
+      endereco: this.formBuilder.group({
+        cep: [null, [Validators.required]],
+        numero: [null, [Validators.required]],
+        complemento: [null, [Validators.required]],
+        rua: [null, [Validators.required]],
+        bairro: [null, [Validators.required]],
+        cidade: [null, [Validators.required]],
+        estado: [null, [Validators.required]]
+      })
+    });
   }
 
+  onSubmit() {
+
+    console.log(this.form.value)
+    
+    if (this.form.valid) {
+
+      this.http.post('https://httpbin.org/post', JSON.stringify(this.form.value))
+        .subscribe(dados => { 
+          console.log(dados)
+          //this.reset();
+        },
+        (error: any) => alert('error')
+      );
+    } else {
+      this.verificaValidacoesForm(this.form);
+    }
+  }
+
+  verificaValidacoesForm(formGroup: FormGroup) {
+    let campos = formGroup.controls;
+
+    Object.keys(campos).forEach(campo => {
+      let control = formGroup.get(campo);
+      control.markAsDirty();
+      if (control instanceof FormGroup) {
+        this.verificaValidacoesForm(control);
+      }     
+    });
+  }
+
+  verificaValidTouched(campo: string) {
+
+    return !this.form.get(campo).valid && (this.form.get(campo).touched || this.form.get(campo).dirty); 
+
+    //return !campo.valid && campo.touched;
+  }
+
+  verificaEmailInvalido() {
+    
+    let campoEmail = this.form.get('email');
+    
+    if (campoEmail.errors) {
+      return campoEmail.errors['email'] && campoEmail.touched;
+    }
+  }
+
+  aplicaCss(campo) {
+
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+    }
+  }
+
+  consultaCEP() {
+
+    let cep = this.form.get('endereco.cep').value;
+
+    // Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+
+    // Verifica se campo cep possui valor informado.
+    if (cep !== '') {
+      // Expressão regular para validar o CEP.
+      const validacep = /^[0-9]{8}$/;
+      
+      // Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        
+        //this.resetaDadosForm();
+        
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          .map(dados => dados.json())
+          .subscribe(dados => this.populaDadosForm(dados))
+      }
+    }
+  }
+
+  populaDadosForm(dados) {
+
+    this.form.patchValue({
+      endereco: {
+        //cep: dados.cep,        
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        complemento: dados.complemento,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+
+    this.form.get('nome').setValue('Yago') 
+  }
+
+  resetaDadosForm() {
+
+    this.form.patchValue({
+      endereco: {
+        cep: null,
+        numero: null,
+        complemento: null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
+  }
+
+  reset() {
+    this.form.reset();
+  }
 }
--- src/app/data-form/data-form.module.ts
+++ src/app/data-form/data-form.module.ts
@@ -1,13 +1,17 @@
+import {HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { DataFormComponent } from './data-form.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
+    SharedModule,
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
   declarations: [
     DataFormComponent
